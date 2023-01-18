4261d9ee132ae833dbee88ea526bbe74bfe000eb
akskflwn <akskflwn@gmail.com>
2023-01-18 14:46:56 +0900

    빌드 성공

--- src/test/java/com/test/project/DemoApplicationTests.java
+++ src/test/java/com/test/project/DemoApplicationTests.java
@@ -1,9 +1,9 @@
-package google.demo;
+package com.test.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
